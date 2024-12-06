e35cbb6e90578871fbdbd1de842f74afd87007d2
Ömer Çıkan <omercikan36@gmail.com>
2024-12-06 22:15:48 +0300

    New year opening animation added

---
+++ src/ts/components/new-year.ts
@@ -0,0 +1,22 @@
+//!Dom content loaded show new year animate showed and removed five secondas later !//
+document.addEventListener('DOMContentLoaded', () => {
+    const getAnimation = sessionStorage.getItem('new-year-animate')
+
+    if(!getAnimation && window.innerWidth <= 430) {
+        const animationElement = document.querySelector<HTMLDivElement>('.new-year-animate');
+        const htmlElement = document.querySelector<HTMLBodyElement>('html');
+        animationElement.style.display = 'block';
+        htmlElement.style.overflow = 'hidden';
+
+        setTimeout(() => {
+            sessionStorage.setItem('new-year-animate', 'true');
+            htmlElement.style.overflowY = 'visible'
+            htmlElement.style.overflowX = 'hidden'
+            animationElement.remove();
+        }, 5000);
+    } else {
+        const animationElement = document.querySelector<HTMLDivElement>('.new-year-animate');
+        animationElement.remove();
+    }
+})
+//!Dom content loaded show new year animate showed and removed five secondas later !//
